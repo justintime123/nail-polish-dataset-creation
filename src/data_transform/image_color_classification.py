bd98f5da54f4b62b7464342424635a573d0e388f
jchassi1 <jchassi1@binghamton.edu>
2024-10-24 13:36:08 -0400

    Saving work for color classification. Added logic to translate HSL values into color categories, and started testing on sample images

--- src/data_transform/image_color_classification.py
+++ src/data_transform/image_color_classification.py
@@ -3,6 +3,11 @@
 #Median or Gaussian Blur had the best results
 import cv2
 import numpy as np
+import colorsys
+import math
+import os
+from scipy.stats import false_discovery_control
+
 
 def gaussian_blur(image, kernel_size):
     return cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
@@ -17,14 +22,14 @@ def median_blur(image, kernel_size):
     return cv2.medianBlur(image, kernel_size)
 
 def get_image(img_path):
-    return cv2.imread(image_path)
+    return cv2.imread(img_path)
 
 def crop_image(img):
-    #Return cropped image according to preset proportions
+    # Return cropped image according to preset proportions
     # https://stackoverflow.com/questions/15589517/how-to-crop-an-image-in-opencv-using-python
     # Take certain proportion from left, right, top and bottom
     # Use same proportions for all images
-    # I figured out what proportions to cut for mt to remove whitespace, and will apply same proportional cropping to OPI
+    # I figured out what proportions to cut for morgan taylor to remove whitespace, and will apply same proportional cropping to OPI
 
     # how much to crop relative to total x and y
     x_crop = 295 / 1680
@@ -37,7 +42,7 @@ def crop_image(img):
     y_max = int(width - crop_width)
 
     # Dimensions
-    print('Original dimensions: ', img.shape)
+    #print('Original dimensions: ', img.shape)
     img = img[crop_length:x_max, crop_width:y_max]
     return img
 
@@ -49,7 +54,7 @@ def get_dominant_color_using_k_means(img):
     # Use K-means to find dominant color
     # https://stackoverflow.com/a/50900494
     data = np.reshape(img, (-1, 3))
-    print(data.shape)
+    #print(data.shape)
     data = np.float32(data)
 
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
@@ -76,6 +81,88 @@ def get_dominant_color_in_image(img_path):
     #Return dominant color in RGB
     return dominant_color
 
+def convert_rgb_to_color_name(rgb_value):
+    #First convert to HSL values, where h is in degrees and l,s are in %
+    #HSL is easier for distinguishing between neutrals and non-neutrals
+    r,g,b = rgb_value
+    r,g,b = r/255.0, g/255.0, b/255.0
+    hue,lightness,saturation = colorsys.rgb_to_hls(r,g,b)
+
+    #Set-up for calculation
+    # Convert hue to number of degrees
+    hue = hue * 360
+
+    print(f"HSL colors: {hue, saturation, lightness}")
+
+    #In HSL model, "neutrals" are considered low saturation
+    #moving luma to the lower end adds black to color, while the higher end adds white
+    #https://vanseodesign.com/web-design/hue-saturation-and-lightness/#:~:text=Saturation%20refers%20to%20how%20pure,to%20a%2075%25%20saturated%20hue.:
+    #saturation determines amount of gray in an image, where 1 is no gray (pure) and 0 is all gray
+
+    #Changed the inspo code:
+    #Color vs. Saturation: https://munsell.com/color-blog/difference-chroma-saturation/
+    #https://photo.stackexchange.com/questions/14820/what-do-hue-chroma-saturation-value-tones-tints-shade-etc-mean
+    #used this website to help determine color ranges: https://hslpicker.com/#c93644c7
+
+    #https://stackoverflow.com/questions/75838909/how-to-get-rough-boundaries-to-classify-colors-into-named-colors
+    unsaturated = saturation < 0.15
+    if unsaturated:
+        if lightness < 0.1:
+            return "black"
+        elif lightness < 0.9:
+            return "gray"
+        else:
+            return "white"
+
+    color = ""
+
+    #Determining color for non-neutrals, using hue
+    #if lightness = 0.5, the color is considered saturated
+    #increasing lightness adds white/makes color lighter
+    #e.g., adding white to red range creates pink
+    if hue < 8:
+        color = "red"
+    elif hue <37:
+        if lightness < 0.15:
+            color =  "brown"
+        else:
+            color =  "orange"
+    elif hue < 71:
+        color = "yellow"
+    elif hue < 173:
+        color = "green"
+    elif hue < 263:
+        color = "blue"
+    elif hue < 293:
+        color = "purple"
+    else:
+        color = "red"
+
+    #Classification: pink is either magenta (hot pink) or a lighter shade of red (white added to red)
+    #for pink, high lightness and certain range of saturation below 0.5
+    if color== "red" and lightness>0.7 and saturation > 0.5:
+        color = "pink"
+
+    return color
+
+
+def classify_dominant_color_in_img(image_path):
+    print(f"***** {image_path} *****")
+    dominant_img = get_dominant_color_in_image(image_path)
+    print('Dominant color is: rgb({})'.format(dominant_img))
+    print("Color name: ", convert_rgb_to_color_name(dominant_img))
+
+def list_files(dir):
+    #https://stackoverflow.com/questions/19932130/iterate-through-folders-then-subfolders-and-print-filenames-with-path-to-text-f
+    r = []
+    subdirs = [x[0] for x in os.walk(dir)]
+    for subdir in subdirs:
+        files = os.walk(subdir).__next__()[2]
+        if (len(files) > 0):
+            for file in files:
+                r.append(os.path.join(subdir, file))
+    return r
+
 
 
 #Sources:
@@ -87,9 +174,30 @@ if __name__=='__main__':
     opi_image_path = 'sample_images/opi/'
     morgan_taylor_path = 'sample_images/morgan_taylor/'
 
-    image_path = morgan_taylor_path + 'all_tied_up_with_a_bow.webp'
-    dominant_img = get_dominant_color_in_image(image_path)
-    print('Dominant color is: rgb({})'.format(dominant_img))
+    file_list = list_files(morgan_taylor_path)
+    for file in file_list:
+        if ".webp" in file:
+            classify_dominant_color_in_img(file)
+
+    # #https://stackoverflow.com/questions/72649116/best-way-to-loop-through-files
+    # for img_name in morgan_taylor_imgs:
+    #     image_path = morgan_taylor_path + img_name
+    #     print(f"***** {img_name} *****")
+    #     dominant_img = get_dominant_color_in_image(image_path)
+    #     print('Dominant color is: rgb({})'.format(dominant_img))
+    #     print("Color name: ", convert_rgb_to_color_name(dominant_img))
+    #
+    # for img_name in opi_imgs:
+    #     image_path = opi_image_path + img_name
+    #     print(f"***** {img_name} *****")
+    #     dominant_img = get_dominant_color_in_image(image_path)
+    #     print('Dominant color is: rgb({})'.format(dominant_img))
+    #     print("Color name: ", convert_rgb_to_color_name(dominant_img))
+
+    # image_path = morgan_taylor_path + 'shine_on_morgan_taylor.webp'
+    # dominant_img = get_dominant_color_in_image(image_path)
+    # print('Dominant color is: rgb({})'.format(dominant_img))
+    # print("Color name: ", convert_rgb_to_color_name(dominant_img))
    #  # img = cv2.imread(image_path)
    #
    #
