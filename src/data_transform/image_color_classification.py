e1ea816f772de70faf003e0d230aa5116d5a40a1
jchassi1 <jchassi1@binghamton.edu>
2025-01-03 20:39:11 -0500

    cleanup of image_color_classification, added convert_rgb_triplet_to_color_family method

--- src/data_transform/image_color_classification.py
+++ src/data_transform/image_color_classification.py
@@ -83,11 +83,82 @@ def get_dominant_color_in_image(img_path):
     #Return dominant color in RGB
     return dominant_color
 
+def get_dominant_color_in_np_image(img):
+    #img_transform = transform_image(img)
+    #Default OpenCV uses BGR format - convert to RGB
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = crop_image(img)
+    img_blur = apply_blur(img)
+    dominant_color = get_dominant_color_using_k_means(img_blur)
+    #Return dominant color in RGB
+    return dominant_color
+
+def get_color_name_using_euclidean_distance(rgb):
+
+    rgb_colors_by_family = {
+       'red': (255,44,44),
+        'maroon': (85,0,0),
+        'burgundy': (102,0,51),
+        'rose': (255,0,128),
+        'magenta': (255,0,255),
+        'periwinkle': (204, 204, 255),
+        'violet': (128,0,255),
+        'dark purple': (52, 21, 57),
+        'blue': (0,0,255),
+        'azure': (0,128,255),
+        'cyan': (0,255,255),
+        'spring green': (0,255,128),
+        'green': (0,255,0),
+        'chartreuse': (128, 255, 0),
+        'yellow': (255,255,0),
+        'gold': (239, 191, 4),
+        'orange': (255,128,0),
+        'nude orange': (247, 217, 188),
+        'coral': (255,133,89),
+        'light pink': (255, 181, 192),
+        'pink': (255, 141, 161),
+        'hot pink': (255, 70, 162),
+        'fuscia': (255,0,255),
+        'light gray': (211, 211, 211),
+        'rose gold': (222, 161, 147),
+        'brown': (137, 81, 41),
+        'off-white': (242, 240, 239),
+        'black': (0, 0, 0)}
+
+
+    # algorithm to match closest color using Euclidean distance
+    # Src: https://stackoverflow.com/questions/9694165/convert-rgb-color-to-english-color-name-like-green-with-python
+    #For pure neutrals, R=G=B
+    #For near neutrals, r,g and b values are close to each other
+    #For simplicity, I'll define near-neutrals as when r,g and b values are within 50 values of each other
+
+    #Neutral test
+    r_g_distance = abs(rgb[0] - rgb[1])
+    r_b_distance = abs(rgb[0] - rgb[2])
+    b_g_distance = abs(rgb[1] - rgb[2])
+    if r_g_distance<=55 and r_b_distance<=55 and b_g_distance<=55:
+        return 'neutral'
+
+    #If not, test for non-neutral categories
+    names_by_distance = {}
+    for name, rgb_value in rgb_colors_by_family.items():
+        r_c, g_c, b_c = rgb_value
+        r_distance = (r_c - rgb[0]) ** 2
+        g_distance = (g_c - rgb[1]) ** 2
+        b_distance = (b_c - rgb[2]) ** 2
+        euclidean_distance = r_distance + g_distance + b_distance
+        names_by_distance[(euclidean_distance)] = name
+
+    #Return color name corresponding to minimum distance
+    return names_by_distance[min(names_by_distance.keys())]
+
 def convert_rgb_to_hsl(rgb_value):
+    if rgb_value is None or len(rgb_value)<3:
+        return None
     # First convert to HSL values, where h is in degrees and l,s are in %
     # HSL is easier for distinguishing between neutrals and non-neutrals
     r, g, b = rgb_value
-    r, g, b = r / 255.0, g / 255.0, b / 255.0
+    r, g, b = int(r) / 255.0, int(g) / 255.0, int(b) / 255.0
     hue, lightness, saturation = colorsys.rgb_to_hls(r, g, b)
 
     # Set-up for calculation
@@ -161,6 +232,12 @@ def convert_hsl_to_color_name(hsl_value):
 
     return color
 
+def convert_rgb_color_to_color_family(rgb_triplet):
+    if rgb_triplet is None or len(rgb_triplet)<3:
+        return ''
+    dominant_hsl_color = convert_rgb_to_hsl(rgb_triplet)
+    new_color_name = convert_hsl_to_color_name(dominant_hsl_color)
+    return new_color_name
 
 def classify_dominant_color_in_img(image_path):
     file_name = image_path.split("/")[-1]
@@ -170,9 +247,10 @@ def classify_dominant_color_in_img(image_path):
     dominant_rgb_color = get_dominant_color_in_image(image_path)
     dominant_hsl_color = convert_rgb_to_hsl(dominant_rgb_color)
     new_color_name = convert_hsl_to_color_name(dominant_hsl_color)
+    new_color_name_from_euclidean_distance = get_color_name_using_euclidean_distance(dominant_rgb_color)
 
     return {'product_name': product_name, 'dominant_color_rgb': dominant_rgb_color, 'dominant_color_hsl': dominant_hsl_color,
-            'original_color_name': original_color_name, 'new_color_name': new_color_name}
+            'original_color_name': original_color_name, 'new_color_name_from_hsl': new_color_name, 'new_color_name_from_eucl': new_color_name_from_euclidean_distance}
 
 def list_files(dir):
     #https://stackoverflow.com/questions/19932130/iterate-through-folders-then-subfolders-and-print-filenames-with-path-to-text-f
@@ -212,35 +290,3 @@ def test_classification_on_sample_images():
 
 if __name__=='__main__':
     test_classification_on_sample_images()
-   #
-   #  #Apply blur to reduce noise in shimmers and other polishes with particles
-   #  #https://medium.com/@henriquevedoveli/blur-in-image-processing-an-introductory-guide-88a9550985e7
-   #  img_gaussian_blur = gaussian_blur(img, 15)
-   #  # img_box_blur = box_blur(img, 15)
-   #  # img_bilateral_blur = bilateral_blur(img, d=9, sigma_color=75, sigma_space=75)
-   #  img_median_blur = median_blur(img, 5)
-   #
-   #  #Resize
-   #  #img_gaussian_blur = cv2.resize(img_gaussian_blur, (50, 50))
-   #  #cv2.imshow('Gaussian', img_gaussian_blur)
-   #
-   #  #open_cv_image = np.array(img_gaussian_blur)
-   #  #open_cv_image = open_cv_image[:, :, ::-1].copy()
-   #  #img = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2RGB)
-   #
-   #  #resize_factor = 0.75
-   #  # k_means = KMeans(n_clusters=4)
-   #  # k_means.fit(img_gaussian_blur)
-   #  #
-   #  # colors = np.asarray(k_means.cluster_centers_, dtype='uint8')
-   #
-   #
-   #  cv2.imshow('Original', img)
-   #  cv2.imshow('Gaussian', img_gaussian_blur)
-   # # cv2.imshow('Box', img_box_blur)
-   #  # cv2.imshow('Bilateral', img_bilateral_blur)
-   #  cv2.imshow('Median', img_median_blur)
-   #
-   #  cv2.waitKey(0)
-   #  cv2.destroyAllWindows()
-
