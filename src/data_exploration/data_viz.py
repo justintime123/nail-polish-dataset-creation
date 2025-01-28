ca9ed9bba26e48e011a27f3e767e9111c3586e9b
jchassi1 <jchassi1@binghamton.edu>
2025-01-27 23:55:43 -0500

    Replaced double bar chart with 2 pie charts, cleaned up code and added 2 dashtables/corresponding callback functions

--- src/data_exploration/data_viz.py
+++ src/data_exploration/data_viz.py
@@ -1,9 +1,7 @@
 import dash.exceptions
 
-from src.dataset_creation.data_transform import morgan_taylor_nail_lacquer, opi_products
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objs as go
 import numpy as np
 from config import PROCESSED_DATA_PATH
 from dash import Dash, html, dcc, callback, Output, Input, dash_table
@@ -15,7 +13,7 @@ def get_data():
     df = df.rename(columns={'new_color': 'color', 'primary_finish': 'finish'})
     return df
 
-def get_bar_chart_data(variable):
+def get_bucketed_data(variable):
     data = get_data()
 
     # group any categories that belong to one brand but not another in seperate "other" category
@@ -41,7 +39,7 @@ def get_bar_chart_data(variable):
 
 def get_double_bar_chart_by_variable(variable):
 
-    data = get_bar_chart_data(variable)
+    data = get_bucketed_data(variable)
 
     agg_data = data.groupby(['brand', f"{variable}_bucket"])[f"{variable}_bucket"].count().reset_index(name='count')
 
@@ -61,48 +59,76 @@ def get_double_bar_chart_by_variable(variable):
     #fig.add_trace(go.Histogram(histfunc="count", x=data[variable], color="brand"))
     return fig
 
-def display_pie_chart():
-    data = get_data()
+def get_pie_chart(brand, variable):
+    data = get_bucketed_data(variable)
+    data = data[data['brand']==brand]
+
+    count_of_each_color = data.groupby(['brand', f"{variable}_bucket"])[f"{variable}_bucket"].count().reset_index(name='count')
+
     #https://stackoverflow.com/questions/72852058/python-how-to-plot-a-frequency-pie-chart-with-one-column-using-plotly-express
     #Apply groupby to dataframe, then pass in new df to px.pie
-    opi = data[data['brand']=='OPI']
-    count_of_each_color = opi.groupby(['color'])['color'].count().reset_index(name='count')
-    fig = px.pie(count_of_each_color, values='count', names='color', title='Breakdown by Color')
-    fig.show()
+    fig = px.pie(count_of_each_color, values='count', names=f"{variable}_bucket",
+                 color=f"{variable}_bucket",
+                 color_discrete_map={'black (neutral)': 'black',
+                                     'blue': 'cornflowerblue',
+                                     'red': 'crimson',
+                                     'orange': 'orange',
+                                     'pink (light red)': 'pink',
+                                     'gray (neutral)': 'lightgrey',
+                                     'pink (magenta)': 'magenta',
+                                     'yellow': 'yellow',
+                                     'green': 'green',
+                                     'purple': 'darkviolet',
+                                     'white (neutral)': 'white',
+                                     'OTHER': 'gray',
+                                     'UNMAPPED': 'silver'
+                                     },
+                 title=brand)
+    return fig
 
 app = Dash()
 app.layout = html.Div(children = [
     html.H1(children='Comparison of Nail Polish Attributes Between Brands'),
     html.Div(children='Select attribute below:'),
     dcc.Dropdown(['color', 'finish'], 'color', id='dropdown'),
-    dcc.Graph(figure={}, id='output_graph'),
-    html.Div(id='table_container')
+    html.Div(children=[
+        dcc.Graph(figure={}, id='morgan_taylor_pie_chart', style={'width': '48%', 'display': 'inline-block'}),
+        dcc.Graph(figure={}, id='opi_pie_chart', style={'width': '48%', 'margin-left': '4%', 'display': 'inline-block'}),
+    ]),
+    html.Div(children = [
+        html.Div(id='morgan_taylor_table_container', style={'width': '48%', 'display': 'inline-block'}),
+        html.Div(id='opi_table_container', style={'width': '48%', 'margin-left': '4%','display': 'inline-block'})
+    ])
 ])
 
 @callback(
-    Output(component_id='output_graph', component_property='figure'),
+    Output(component_id='morgan_taylor_pie_chart', component_property='figure'),
+    Output(component_id='opi_pie_chart', component_property='figure'),
     Input(component_id='dropdown', component_property='value')
 )
-def update_chart(val_chosen):
-    fig = get_double_bar_chart_by_variable(val_chosen)
-    return fig
+def update_pie_charts(val_chosen):
+    return get_pie_chart('Morgan Taylor', val_chosen), get_pie_chart('OPI', val_chosen)
 
-#https://stackoverflow.com/questions/71320914/can-i-add-an-onclick-function-to-a-plotly-express-bar-chart
-@callback(
-    Output(component_id='table_container', component_property='children'),
-    [Input(component_id='output_graph', component_property='clickData'),
-     Input(component_id='dropdown', component_property='value')]
-)
-def update_table(clickData, value):
+def update_dashtable(clickData, value, brand):
     if not clickData:
         raise dash.exceptions.PreventUpdate
 
     bucket_selected = clickData["points"][0]["label"]
-    df = get_bar_chart_data(value)
+    df = get_bucketed_data(value)
     #filter dataframe according to selected category
-    df = df[df[f"{value}_bucket"]==bucket_selected]
+    df = df[(df[f"{value}_bucket"]==bucket_selected) & (df['brand']==brand)]
+    display_cols = ['product_name', 'color', 'finish']
+    #display_cols = ['product_name', 'finish', 'product_type', f"{value}_bucket"]
+    df = df[display_cols]
+    df = df.rename(columns={'product_name': 'Product Name', 'color': 'Color', 'finish': 'Finish'})
     table = dash_table.DataTable(data=df.to_dict('records'),
-                                 page_size=5)
+                                 page_size=5,
+                                 style_header={
+                                     'backgroundColor': 'snow',
+                                     'fontWeight': 'bold',
+                                 },
+                                 style_data = {})
+
                                  # tooltip_header={'swatch_rgb_color': 'RGB value obtained from product image. This RGB value was mapped to a color'},
                                  # style_cell={'overflow': 'hidden',
                                  #             'textOverflow': 'ellipses',
@@ -114,6 +140,57 @@ def update_table(clickData, value):
     return table
 
 
+@callback(
+    Output(component_id='morgan_taylor_table_container', component_property='children'),
+    [Input(component_id='morgan_taylor_pie_chart', component_property='clickData'),
+     Input(component_id='dropdown', component_property='value')]
+)
+def update_table(clickData, value):
+    return update_dashtable(clickData, value, 'Morgan Taylor')
+
+@callback(
+    Output(component_id='opi_table_container', component_property='children'),
+    [Input(component_id='opi_pie_chart', component_property='clickData'),
+     Input(component_id='dropdown', component_property='value')]
+)
+def update_table(clickData, value):
+    return update_dashtable(clickData, value, 'OPI')
+
+# @callback(
+#     Output(component_id='output_graph', component_property='figure'),
+#     Input(component_id='dropdown', component_property='value')
+# )
+# def update_chart(val_chosen):
+#     fig = get_double_bar_chart_by_variable(val_chosen)
+#     return fig
+
+#https://stackoverflow.com/questions/71320914/can-i-add-an-onclick-function-to-a-plotly-express-bar-chart
+# @callback(
+#     Output(component_id='table_container', component_property='children'),
+#     [Input(component_id='output_graph', component_property='clickData'),
+#      Input(component_id='dropdown', component_property='value')]
+# )
+# def update_table(clickData, value):
+#     if not clickData:
+#         raise dash.exceptions.PreventUpdate
+#
+#     bucket_selected = clickData["points"][0]["label"]
+#     df = get_bucketed_data(value)
+#     #filter dataframe according to selected category
+#     df = df[df[f"{value}_bucket"]==bucket_selected]
+#     table = dash_table.DataTable(data=df.to_dict('records'),
+#                                  page_size=5)
+#                                  # tooltip_header={'swatch_rgb_color': 'RGB value obtained from product image. This RGB value was mapped to a color'},
+#                                  # style_cell={'overflow': 'hidden',
+#                                  #             'textOverflow': 'ellipses',
+#                                  #             'maxWidth': 0},
+#                                  # tooltip_delay=0,
+#                                  # tooltip_duration=None)
+#     #TODO: Add hyperlinked emojis to link products
+#     #TODO: change color of swatch_hex_colors using style_data_conditional and columns. Highlight cells with empty values
+#     return table
+
+
 
 
 
@@ -122,4 +199,4 @@ if __name__=='__main__':
     app.run(debug=True)
     #display_double_bar_chart_by_variable("new_color")
     #display_double_bar_chart_by_variable("primary_finish")
-    #display_pie_chart()
+    # display_pie_chart('OPI', 'color')
